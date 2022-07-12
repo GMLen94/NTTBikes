e32c043194a1842a522352f8ce7ca325ba94a8d7
elio <Elio.Campanile@emea.nttdata.com>
2022-07-12 16:47:21 +0200

    prime classi

---
+++ NTTBikes/Models/Bike.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BikesByBDEGLR.Models
+{
+    public class Bike
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        public string Type { get; set; }
+
+        public bool IsWorking { get; set; }
+
+        [ForeignKey("BikeStation")]
+        public Guid IdStation { get; set; }
+        public BikeStation Station { get; set; }
+
+        public bool LockOn { get; set; }
+
+
+    }
+}
---
+++ NTTBikes/Models/BikeStation.cs
@@ -0,0 +1,9 @@
+﻿namespace BikesByBDEGLR.Models
+{
+    public class BikeStation
+    {
+        public Guid Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
