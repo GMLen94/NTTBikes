a6c29b930fb6f7ae061d07195d0664bd47008ba7
gbelda <giovannimelville.belda@nttdata.com>
2022-07-12 18:04:11 +0200

    Add home css styling

--- NTTBikes/Data/AppDbContext.cs
+++ NTTBikes/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace NTTBikes.Data
 {
     public class AppDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
+        
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
 
