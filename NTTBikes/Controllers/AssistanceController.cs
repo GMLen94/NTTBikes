ae1f309047c0b3338e2db2efc95bb0291d6b5825
elio <Elio.Campanile@emea.nttdata.com>
2022-07-12 18:02:21 +0200

    aggiunta assistenza

---
+++ NTTBikes/Controllers/AssistanceController.cs
@@ -0,0 +1,22 @@
+﻿using BikesByBDEGLR.Models;
+using Microsoft.AspNetCore.Mvc;
+using NTTBikes.Data;
+
+namespace NTTBikes.Controllers
+{
+    public class AssistanceController : Controller
+    {
+        AppDbContext appDbContext;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Confirmed(Guid Id)
+        {
+            appDbContext.Bikes.Find(Id).IsWorking = false;
+            //e poi si dovrebbe chiamare l'assistenza per davvero
+            return View();
+        }
+    }
+}
